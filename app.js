c70f4c131b964f6304fc54cf280f3233f654684e
jethro13 <jethro13011@hotmail.fr>
2017-01-29 19:31:38 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+document.getElementById('toggle').addEventListener('onclick', function() {
+  document.getElementsByClassName('left').classList.add('hide');
+});
+});
